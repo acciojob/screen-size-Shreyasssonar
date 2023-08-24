6b8c94593d5c7250d7fba642e69c353346b004ae
Shreyas <121279572+Shreyasssonar@users.noreply.github.com>
2023-08-24 08:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+   function updateSize() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      
+      const widthValue = document.getElementById("widthValue");
+      const heightValue = document.getElementById("heightValue");
+      
+      widthValue.textContent = width;
+      heightValue.textContent = height;
+    }
+    
+    window.addEventListener("resize", updateSize);
+    updateSize(); // Initial call to set the initial window size
+    
+    // Prevent the default behavior of the 'resize' event to minimize browser performance issues
+    let resizeTimeout;
+    window.addEventListener("resize", () => {
+      clearTimeout(resizeTimeout);
+      resizeTimeout = setTimeout(updateSize, 250);
+    });
